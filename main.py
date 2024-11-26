8465d7b98f34072a48d4666b2b888c055c3e8c0e
Elif Vural <elifvural019@gmail.com>
2024-11-26 10:27:57 +0300

    makine öğrenimi modelini eğitme

--- main.py
+++ main.py
@@ -2,16 +2,18 @@ import os
 import librosa
 import matplotlib.pyplot as plt
 import numpy as np
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score, f1_score
 
+# Ses verilerinin bulunduğu dizinin yolu
+dataset_path = './dataset'
 
-# Ses verilerinin bulundugu dizinin yolu
-dataset_path='./dataset'
+# Özellikler ve etiketler
+x = []  # MFCC özelliklerini saklayacak olan liste
+y = []  # Etiketleri saklayacak olan liste
 
-# Ozellikler ve etiketler
-x=[] # MFCC ozelliklerini saklayacak olan liste
-y=[] # Etiketleri saklayacak olan liste
-
-# Ses dosyalarini yukleyelim ve MFCC ozelliklerini cikaralim
+# Ses dosyalarını yükleyelim ve MFCC özelliklerini çıkaralım
 for label, speaker_folder in enumerate(os.listdir(dataset_path)):
     speaker_folder_path = os.path.join(dataset_path, speaker_folder)
     
@@ -32,37 +34,35 @@ for label, speaker_folder in enumerate(os.listdir(dataset_path)):
                 x.append(mfcc)
                 y.append(label)  # Etiket, klasör sırasına göre atanır
 
-# Veriyi numpy arraylerine donusturelim
+# Veriyi numpy arraylerine dönüştürelim
 X = np.array(x)
 y = np.array(y)
 
 # Veriyi kontrol edelim
-print(f"x shape: {x.shape}")
+print(f"X shape: {X.shape}")
 print(f"y shape: {y.shape}")
 
 # Ses dosyasının yolu
-audio_file = os.path.join('audio', 'kayit1.wav')
+audio_file = './audio/kayit1.wav'
 
 # Ses dosyasını yükleme
-audio_file = './audio/kayit1.wav'  # Düz eğik çizgi ile
 y, sr = librosa.load(audio_file)
 
-# MFCC ozelliklerini cikariyoruz
+# MFCC özelliklerini çıkarıyoruz
 mfcc = librosa.feature.mfcc(y=y, sr=sr, n_mfcc=13)
 
-# MFCC ozelliklerinin boyutlarini kontrol ediyoruz
-print(mfcc.shape)
-
+# MFCC özelliklerinin boyutlarını kontrol ediyoruz
+print(f"MFCC Shape: {mfcc.shape}")
 
 # Ses bilgilerini yazdırma
 print(f"Örnekleme oranı (Sample Rate): {sr}")
 print(f"Sesin toplam uzunluğu (in seconds): {librosa.get_duration(y=y, sr=sr)}")
 
-# Ses verisinin histogramini olusturma
-plt.figure(figsize=(10,6))
-plt.hist(y, bins=100, color='blue', alpha=0.7) #ses verisini histogram olarak cizer.
+# Ses verisinin histogramını oluşturma
+plt.figure(figsize=(10, 6))
+plt.hist(y, bins=100, color='blue', alpha=0.7)  # Ses verisini histogram olarak çizer.
 plt.title("Ses Verisi Histogramı")
 plt.xlabel("Amplitüd")
 plt.ylabel("Frekans")
 plt.grid(True)
-plt.show() #grafigi ekranda goruntuler
+plt.show()  # Grafiği ekranda görüntüler
