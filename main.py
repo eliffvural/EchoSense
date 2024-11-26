48ab52399c7b0f81d14262b2a25bd869c933bcb0
Elif Vural <elifvural019@gmail.com>
2024-11-26 10:32:07 +0300

    test verisi üzerinde tahmin yapma

--- main.py
+++ main.py
@@ -45,7 +45,8 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_
 model = RandomForestClassifier(random_state=42)
 model.fit(X_train, y_train)
 
-
+# Test verisi üzerinde tahmin yapalim
+y_pred = model.predict(X_test)
 
 
 
