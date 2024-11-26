27e1c3c4bd74cf434086d1ae380864f3a3e0d8a6
Elif Vural <elifvural019@gmail.com>
2024-11-26 09:44:24 +0300

    matplotlib.pyplot imported

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import os
 import librosa
+import matplotlib.pyplot as plt
+
 
 # Ses dosyasının yolu
 audio_file = os.path.join('audio', 'kayit1.wav')
 
 # Ses dosyasını yükleme
-audio_file='kayit1.wav'
+audio_file = './audio/kayit1.wav'  # Düz eğik çizgi ile
 y, sr = librosa.load(audio_file)
 
 # Ses bilgilerini yazdırma
@@ -14,9 +16,9 @@ print(f"Sesin toplam uzunluğu (in seconds): {librosa.get_duration(y=y, sr=sr)}"
 
 # Ses verisinin histogramini olusturma
 plt.figure(figsize=(10,6))
-plt.hist(y, bins=100, color='blue', alpha=0.7)
+plt.hist(y, bins=100, color='blue', alpha=0.7) #ses verisini histogram olarak cizer.
 plt.title("Ses Verisi Histogramı")
 plt.xlabel("Amplitüd")
 plt.ylabel("Frekans")
 plt.grid(True)
-plt.show()
+plt.show() #grafigi ekranda goruntuler
