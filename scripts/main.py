f50c1acbb8b0eb5a56b31bd91f5488980bde317b
Elif Vural <elifvural019@gmail.com>
2024-12-01 12:40:11 +0300

    layout ekleme

--- scripts/main.py
+++ scripts/main.py
@@ -1,9 +1,14 @@
 from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.button import Button
+from kivy.uix.label import Label
 
 class MyApp(App):
     def build(self):
-        return Button(text="Merhaba Dünya!", font_size=24)
+        layout = BoxLayout(orientation='vertical')
+        layout.add_widget(Label(text="Merhaba!"))
+        layout.add_widget(Button(text="Tıklayın!"))
+        return layout
 
 if __name__ == "__main__":
     MyApp().run()
